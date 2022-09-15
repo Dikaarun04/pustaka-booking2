e0d50e13349fe9bebd7963923b1fec12b8992b98
dikaarunazilputra <dikaarun04@gmail.com>
2022-09-15 17:16:39 +0700

    Upload awal project

---
+++ application/controllers/pelatihan1.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class pelatihan1 extends CI_Controller{
+
+    public function __construct()
+    {
+        parent::__construct();
+        //Codeigniter : Write Less Do More
+    }
+
+    function index()
+    {
+    echo"Selamat Belajar WEB PROGRAMMING 2 dengan kelas 12.3A.35";
+    }
+
+}
